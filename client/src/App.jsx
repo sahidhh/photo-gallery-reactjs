09e9ef0dccc3f723b0a80121a486f0f357261d8f
Sahidh <sahidhh5@gmail.com>
2024-08-19 01:41:06 +0530

    added dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div id="container">
+      <div id="nav-bar">
+        <h2>Photo Gallery</h2>
+        <div>
+          <img src="..\assets\filter.png" alt="" />
+          <img src={"client/public/Vector.svg"} alt="" />
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
